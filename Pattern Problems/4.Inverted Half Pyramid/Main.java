1c7792088b1cb8296042889ec69a7ad376c800eb
Nikil kumar <kumarnikil220@gmail.com>
2023-03-27 23:39:51 +0530

    pyramid patterns

---
+++ Pattern Problems/4.Inverted Half Pyramid/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int rows = 4;
+        for (int i = rows; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern Problems/5.Inverted Half pyramid rotated by 180deg/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int n=4;
+        //outer loop
+        for(int i=1;i<=n;i++)
+        {
+            //for blank spaces
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            //for star
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
