575e7112f139a98470f29017705f153a2aceb85a
Nikil kumar <kumarnikil220@gmail.com>
2023-03-27 22:36:06 +0530

    Patterns problems

---
+++ Pattern Problems/1.Solid rectangle/SolidRectangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SolidRectangle {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            // for inner loop
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+ 
+        }
+    }
+}
