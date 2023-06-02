e84f5d7618295cb974da65bc7ee1803c22a52e2d
Nikhil <kumarnikil220@gmail.com>
2023-06-02 17:07:58 +0530

    display LL

---
+++ Linked List/recursive method to print linked list/LL.java
@@ -0,0 +1,29 @@
+public class LL {
+    public static class Node {
+        int data;
+        Node next;
+
+        // constructor
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void display(Node head) {
+        if (head == null)
+            return;
+        System.out.print(head.data + " ");
+        display(head.next);
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(10);
+        Node c = new Node(15);
+        Node d = new Node(20);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        display(a);
+    }
+}
