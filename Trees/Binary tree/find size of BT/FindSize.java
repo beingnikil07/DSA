3f454769a28c8aa971dc20dca08e1320805a3162
Nikhil <kumarnikil220@gmail.com>
2023-10-04 17:23:10 +0530

    organizing files

---
+++ Trees/Binary tree/find size of BT/FindSize.java
@@ -0,0 +1,34 @@
+public class FindSize {
+    public static class Node {
+        int val; // initially it points to zero
+        Node left; // initially points to null
+        Node right; // initially points to null
+        // constructor
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+    //Traversal 
+    public static void preorder(Node root){
+            if(root==null) return;
+            System.out.println(root.val+" ");
+            preorder(root.left);
+            preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right = b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.left = c;
+        a.right = d;
+        Node e = new Node(6);
+        b.right = e;
+        preorder(root);
+    }
+}
