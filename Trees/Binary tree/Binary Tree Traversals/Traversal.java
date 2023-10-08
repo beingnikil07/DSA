4883406a32d9aa833ddea08805b7fa1c80e2d50b
Nikhil <kumarnikil220@gmail.com>
2023-10-08 11:19:55 +0530

    Binary tree traversal

---
+++ Trees/Binary tree/Binary Tree Traversals/Traversal.java
@@ -0,0 +1,57 @@
+public class Traversal {
+    public static class Node {
+        int val; // initially it points to zero
+        Node left; // initially points to null
+        Node right; // initially points to null
+        // constructor
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root == null)
+            return;
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null)
+            return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null)
+            return;
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right = b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.left = c;
+        a.right = d;
+        Node e = new Node(6);
+        Node f = new Node(7);
+        b.left = e;
+        b.right = f;
+        preorder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+    }
+}
