751cc51e7e9cb235bb06b18ddd2b8ab586407505
Nikhil <kumarnikil220@gmail.com>
2023-10-08 10:10:26 +0530

    max node find problem of Binary Tree

---
+++ Trees/Binary tree/find node with max value/MaxNode.java
@@ -0,0 +1,37 @@
+public class MaxNode {
+    public static class Node {
+        int val; // initially it points to zero
+        Node left; // initially points to null
+        Node right; // initially points to null
+        // constructor
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    // Height of the Binary tree
+    public static int height(Node root) {
+        if (root == null || (root.left == null && root.right == null))
+            return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right = b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.left = c;
+        a.right = d;
+        Node e = new Node(6);
+        b.right = e;
+        System.out.println(height(root));
+       
+    }
+}
